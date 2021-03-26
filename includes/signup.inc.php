9e6df9c66a0c8037a0e2ae9ac4c9a7eacfd4f6a7
Mads Skovlund Thaisen <mads.skovlund.thaisen5792@gmail.com>
2021-03-26 12:43:32 +0100

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,4 +3,9 @@ if (isset($_POST['signup-submit'])) {
 
   require 'dbh.inc.php';
 
+$username = $_POST['uid']
+$email = $_POST['mail']
+$password = $_POST['pwd']
+$passwordRepeat = $_POST['pwd-repeat']
+
 }
